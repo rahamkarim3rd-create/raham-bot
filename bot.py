211589d06e95d791a134c0a92e3d9d4799f2c263
rahamkarim3rd-create <rahamkarim3rd@gmail.com>
2026-04-17 06:04:49 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,5 +38,7 @@ def dashboard():
 
     return render_template("dashboard.html", data=data, timeframes=timeframes)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
